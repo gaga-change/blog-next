1bfad949b7a830073c4847653ac780e7b2cc0896
严俊东 <1172482914@qq.com>
2019-04-20 22:18:18 +0800

    分页处理

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const host = 'http://localhost:3000'
 const base = host + '/api'
 
 // 获取文章列表
-export const posts = () => axios.get(`${base}/posts?page=1&pageSize=10`)
+export const posts = params => axios.get(`${base}/posts`, { params })
 
 // 标签、分类，附加最近文章
 export const terms = () => axios.get(`${base}/terms`)
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Layout, List, Avatar, Icon, Typography, Tag, Card } from 'antd'
+import { Layout, List, Icon, Typography, Card, Affix } from 'antd'
 import Head from 'next/head';
 import Link from 'next/link'
 import './index.scss'
@@ -13,21 +13,37 @@ const IconText = ({ type, text }) => (
     <Icon type={type} style={{ marginRight: 8 }} />
     {text}
   </span>
-);
+)
 
+const MyLink = ({ href, ele }) => (
+  <Link href={href}>
+    {ele}
+  </Link>
+)
+
+
+const DEFAULT_PAGE_SIZE = 10
 class Index extends React.Component {
-  static async getInitialProps({ req }) {
-    let res = await Promise.all([posts(), terms()])
+
+  static async getInitialProps({ req, query }) {
+    let { page = 1 } = query
+    let res = await Promise.all([posts({ page, pageSize: DEFAULT_PAGE_SIZE }), terms()])
     let postsData = res[0].data.data
     let other = res[1].data.data
     return {
-      pageSize: 10,
-      count: postsData.count,
-      data: postsData.list,
-      tags: other.tags,
-      categories: other.categories,
-      posts: other.posts // 最新
+      pageSize: DEFAULT_PAGE_SIZE, // 每页条数
+      count: postsData.count, // 总条数
+      data: postsData.list, // post列表
+      tags: other.tags, // tag 列表
+      categories: other.categories, // 分类列表
+      posts: other.posts, // 最新 post
+      page: postsData.page, // 当前页面
+      pages: postsData.pages, // 总页数
     }
+  }
+
+  componentDidMount() {
+    console.log(this.props)
   }
 
   render() {
@@ -52,10 +68,17 @@ class Index extends React.Component {
                 size="large"
                 dataSource={props.data}
                 pagination={{
-                  onChange: (page) => {
-                    console.log(page);
+                  itemRender: (page, type, originalElement) => {
+                    if ((page === 0 && type === 'prev') || (page === props.pages && type === 'next')) return originalElement
+                    if (type == 'page') {
+                      return (<MyLink href={`/?page=${page}`} ele={originalElement} />)
+                    }
+                    return (<MyLink href={`/?page=${page}`} ele={originalElement} />)
+
                   },
+                  total: props.count,
                   pageSize: props.pageSize,
+                  current: props.page
                 }}
                 renderItem={item => (
                   <List.Item
@@ -63,25 +86,20 @@ class Index extends React.Component {
                     actions={[<IconText type="read" text="0" />, <IconText type="like-o" text="0" />, <IconText type="message" text="0" />,
                     <span className="item-tags"><Icon type="tags" />{(<span>{item.tags.join('/')}</span>)}</span>
                     ]}
-                    // extra={(<div>{item.tags.map(tag => <Tag key={tag} color="cyan">{tag}</Tag>)}</div>)}
-                    // actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
                     extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
                   >
                     <List.Item.Meta
                       title={(<Link href="/detail"><a>{item.title}</a></Link>)}
                       description={item.intro + '...'}
                     />
-
-                    {/* <Tag color="gold">gold</Tag> */}
                   </List.Item>
                 )}
               />
             </Content>
             <Sider className="sider">
-              <div>
+              <Affix>
                 <Card
-                  title="分类"
-                // style={{ width: 300 }}
+                  title={(<span>  <Icon type="folder" />分类 </span>)}
                 >
                   {props.categories.map(category => (
                     <div key={category}>
@@ -92,8 +110,7 @@ class Index extends React.Component {
                 <div style={{ marginTop: 16 }}>
                   <Card
                     size="small"
-                    title="标签"
-                  // style={{ width: 300 }}
+                    title={(<span>  <Icon type="tag" />标签 </span>)}
                   >
                     {props.tags.map(tag => (
                       <span key={tag} className="tags">
@@ -102,7 +119,19 @@ class Index extends React.Component {
                     ))}
                   </Card>
                 </div>
-              </div>
+                <div style={{ marginTop: 16 }}>
+                  <Card
+                    size="small"
+                    title={(<span>  <Icon type="file" />最新文章 </span>)}
+                  >
+                    {props.posts.map(post => (
+                      <div key={post.id}>
+                        <Link href="/"><a>{post.title}</a></Link>
+                      </div>
+                    ))}
+                  </Card>
+                </div>
+              </Affix>
             </Sider>
           </Layout>
           <Footer className="footer">
