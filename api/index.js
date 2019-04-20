6c3fc540adaea02347bbdecf1473f4817b959413
严俊东 <1172482914@qq.com>
2019-04-20 21:29:56 +0800

    分类和标签展示

--- api/index.js
+++ api/index.js
@@ -3,4 +3,8 @@ import axios from 'axios'
 const host = 'http://localhost:3000'
 const base = host + '/api'
 
+// 获取文章列表
 export const posts = () => axios.get(`${base}/posts?page=1&pageSize=10`)
+
+// 标签、分类，附加最近文章
+export const terms = () => axios.get(`${base}/terms`)
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import { Layout, List, Avatar, Icon, Typography, Tag } from 'antd'
+import React from 'react'
+import { Layout, List, Avatar, Icon, Typography, Tag, Card } from 'antd'
 import Head from 'next/head';
 import Link from 'next/link'
 import './index.scss'
-import { posts } from '../api'
+import { posts, terms } from '../api'
 
 const { Text, Title } = Typography;
 const { Header, Footer, Sider, Content } = Layout;
@@ -13,59 +14,103 @@ const IconText = ({ type, text }) => (
     {text}
   </span>
 );
-const Index = (props) => (
-  <div>
-    <Head>
-      <title>严俊东 &#8211; 严俊东个人博客</title>
-    </Head>
-    <Layout>
-      <Header>
-        <div className="container">
-          <Title level={3}>严俊东<Text type="secondary" className="title-des"> &#8211; 严俊东个人博客</Text></Title>
-        </div>
-      </Header>
-      <Layout className="container">
-        <Content>
-          <List
-            split={true}
-            itemLayout="vertical"
-            size="large"
-            dataSource={props.data}
-            renderItem={item => (
-              <List.Item
-                key={item.title}
-                actions={[<IconText type="read" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />,
-                <span className="item-tags"><Icon type="tags" />{(<span>{item.tags.join('/')}</span>)}</span>
-                ]}
 
-                // extra={(<div>{item.tags.map(tag => <Tag key={tag} color="cyan">{tag}</Tag>)}</div>)}
-                // actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
-                extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
-              >
-                <List.Item.Meta
-                  title={(<Link href="/detail"><a>{item.title}</a></Link>)}
-                  description={item.intro + '...'}
-                />
+class Index extends React.Component {
+  static async getInitialProps({ req }) {
+    let res = await Promise.all([posts(), terms()])
+    let postsData = res[0].data.data
+    let other = res[1].data.data
+    return {
+      pageSize: 10,
+      count: postsData.count,
+      data: postsData.list,
+      tags: other.tags,
+      categories: other.categories,
+      posts: other.posts // 最新
+    }
+  }
 
-                {/* <Tag color="gold">gold</Tag> */}
-              </List.Item>
-            )}
-          />
-        </Content>
-        <Sider>Sider</Sider>
-      </Layout>
-      <Footer>
-        Copyright © 2019<a href="/" rel="nofollow">严俊东</a> | <a rel="nofollow" target="_blank" href="http://www.miitbeian.gov.cn/">浙ICP备17054210号-2 </a>
-      </Footer>
-    </Layout>
-  </div>
-)
+  render() {
+    const { props } = this
+    return (
+      <div className="page-index">
+        <Head>
+          <title>严俊东 &#8211; 严俊东个人博客</title>
+        </Head>
+        <Layout className="layout-total">
+          <Header className="header">
+            <div className="container">
+              <Title level={3}>严俊东<Text type="secondary" className="title-des"> &#8211; 严俊东个人博客</Text></Title>
+            </div>
+          </Header>
+          <Layout className="layout-con container">
+            <Content className="content">
+              <List
+                className="list"
+                split={true}
+                itemLayout="vertical"
+                size="large"
+                dataSource={props.data}
+                pagination={{
+                  onChange: (page) => {
+                    console.log(page);
+                  },
+                  pageSize: props.pageSize,
+                }}
+                renderItem={item => (
+                  <List.Item
+                    key={item.title}
+                    actions={[<IconText type="read" text="0" />, <IconText type="like-o" text="0" />, <IconText type="message" text="0" />,
+                    <span className="item-tags"><Icon type="tags" />{(<span>{item.tags.join('/')}</span>)}</span>
+                    ]}
+                    // extra={(<div>{item.tags.map(tag => <Tag key={tag} color="cyan">{tag}</Tag>)}</div>)}
+                    // actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />, <IconText type="message" text="2" />]}
+                    extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+                  >
+                    <List.Item.Meta
+                      title={(<Link href="/detail"><a>{item.title}</a></Link>)}
+                      description={item.intro + '...'}
+                    />
 
-Index.getInitialProps = async function ({ req }) {
-  let res = await posts()
-  let postsData = res.data.data.list
-  return {
-    data: postsData
+                    {/* <Tag color="gold">gold</Tag> */}
+                  </List.Item>
+                )}
+              />
+            </Content>
+            <Sider className="sider">
+              <div>
+                <Card
+                  title="分类"
+                // style={{ width: 300 }}
+                >
+                  {props.categories.map(category => (
+                    <div key={category}>
+                      <Link href="/"><a>{category}</a></Link>
+                    </div>
+                  ))}
+                </Card>
+                <div style={{ marginTop: 16 }}>
+                  <Card
+                    size="small"
+                    title="标签"
+                  // style={{ width: 300 }}
+                  >
+                    {props.tags.map(tag => (
+                      <span key={tag} className="tags">
+                        <Link href="/"><a>{tag}</a></Link>
+                      </span>
+                    ))}
+                  </Card>
+                </div>
+              </div>
+            </Sider>
+          </Layout>
+          <Footer className="footer">
+            Copyright © 2019<a href="/" rel="nofollow">严俊东</a> | <a rel="nofollow" target="_blank" href="http://www.miitbeian.gov.cn/">浙ICP备17054210号-2 </a>
+          </Footer>
+        </Layout>
+      </div>
+    )
   }
 }
 
