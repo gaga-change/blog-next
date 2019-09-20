0534db8224e475a977c0d621627521ec1bdb2cdf
严俊东 <1172482914@qq.com>
2019-09-20 18:56:18 +0800

    公共头部样式调整、添加导航链接

--- components/LayoutHeader.js
+++ components/LayoutHeader.js
@@ -1,17 +1,36 @@
 import React from 'react'
-import { Typography } from 'antd'
+import { Typography, Menu, Icon } from 'antd'
+
 import Link from 'next/link'
 
 const { Text, Title } = Typography
 class LayoutHeader extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Link href="/">
-          <a>
-            <Title level={3}>严俊东<Text type="secondary" className="title-des"> &#8211; 严俊东个人博客</Text></Title>
-          </a>
-        </Link>
+      <div className="LayoutHeaderComponent">
+        <div className="container">
+          <div className="left-area">
+            <Link href="/">
+              <a>
+                <Title level={3}>严俊东<Text type="secondary" className="title-des"> &#8211; 严俊东个人博客</Text></Title>
+              </a>
+            </Link>
+          </div>
+          <div className="right-area">
+            <Menu mode="horizontal">
+              <Menu.Item key="home">
+                <Link href="/">
+                  首页
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="about">
+                <Link href="/about">
+                  关于我
+                </Link>
+              </Menu.Item>
+            </Menu>
+          </div>
+        </div>
       </div>
     )
   }
