f2f807615cbe560011529a3f02804459d41dfd3a
严俊东 <1172482914@qq.com>
2019-09-23 16:30:57 +0800

    cdn 前缀修正

--- cdn.js
+++ cdn.js
@@ -5,7 +5,7 @@ const config = {
   secretKey: process.env.QINIU_SECRET_KEY,
   bucket: 'yanjd', // 空间名称
   zone: 'Zone_z2', // 机房：华东 Zone_z0 、华北 Zone_z1 、华南 Zone_z2 、北美 Zone_na0
-  prefix: 'blog/_next/', // 附加前缀，可以为空，或者不填
+  prefix: 'blog/_next/static/', // 附加前缀，可以为空，或者不填
 }
 
 upload('./.next/static', config)
