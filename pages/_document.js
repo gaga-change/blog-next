fe7267fb47de5f1d4ee72b24bb4a855225faca39
严俊东 <1172482914@qq.com>
2019-10-15 16:47:27 +0800

    lang="zh" 标记中文

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <html>
+      <html lang="zh">
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta charSet='utf-8' />
