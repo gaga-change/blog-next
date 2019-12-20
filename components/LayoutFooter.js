600dc27e973aec8cf2387dfebb2c8ff16a6e9aae
严俊东 <1172482914@qq.com>
2019-12-20 17:06:14 +0800

    备案号链接修正

--- components/LayoutFooter.js
+++ components/LayoutFooter.js
@@ -6,7 +6,7 @@ class LayoutFooter extends React.Component {
   render() {
     return (
       <div className="footer">
-        Copyright © 2019<Link href="/"><a rel="nofollow">严俊东</a></Link> | <a rel="nofollow" target="_blank" href="http://www.miitbeian.gov.cn/">浙ICP备17054210号</a>
+        Copyright © 2019<Link href="/"><a rel="nofollow">严俊东</a></Link> | <a rel="nofollow" target="_blank" href="//www.beian.miit.gov.cn">浙ICP备17054210号-2</a>
       </div>
     )
   }
